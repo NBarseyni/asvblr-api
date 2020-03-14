0fa2806cf9b01ad90d2f2ca0d15ccd9762357c7a
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-03-14 21:52:56 +0100

    Init Spring project with Hello world on api/example/hello-world

---
+++ src/main/java/com/pa/asvblrapi/AsvblrApiApplication.java
@@ -0,0 +1,13 @@
+package com.pa.asvblrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsvblrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsvblrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pa/asvblrapi/AsvblrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pa.asvblrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsvblrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
