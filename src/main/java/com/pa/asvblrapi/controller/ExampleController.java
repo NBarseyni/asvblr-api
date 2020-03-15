fa6ae5b0349211424b60b80ff7e946c361a30242
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-03-15 13:43:37 +0100

    FIX rename package controllers -> controller

--- src/main/java/com/pa/asvblrapi/controller/ExampleController.java
+++ src/main/java/com/pa/asvblrapi/controller/ExampleController.java
@@ -1,4 +1,4 @@
-package com.pa.asvblrapi.controllers;
+package com.pa.asvblrapi.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
