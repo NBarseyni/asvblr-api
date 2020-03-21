25d332b93815316297cf4df9fd675c4b0eb14cf0
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-03-21 23:50:53 +0100

    ADD Request / Response / Config security / jwt utils
    - ADD dependencies
    - ADD jwtSecret & jwtexpirationMs

---
+++ src/main/java/com/pa/asvblrapi/payload/request/SignupRequest.java
@@ -0,0 +1,36 @@
+package com.pa.asvblrapi.payload.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Getter
+@Setter
+public class SignupRequest {
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 3, max = 30)
+    private String lastName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    private Set<String> role;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+}
---
+++ src/main/java/com/pa/asvblrapi/payload/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.pa.asvblrapi.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
