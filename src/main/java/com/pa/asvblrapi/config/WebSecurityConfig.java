8008317808bff3bd6cbc169c2cd13f86f9e6b58e
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-03-25 13:33:50 +0100

    UPD WebSecurityConfig

--- src/main/java/com/pa/asvblrapi/config/WebSecurityConfig.java
+++ src/main/java/com/pa/asvblrapi/config/WebSecurityConfig.java
@@ -54,8 +54,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                //.antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/test/**").hasAuthority("SUBSCRIPTION_MANAGEMENT")
+                .antMatchers("/api/users/**").hasAuthority("READ_PRIVILEGE")
                 .anyRequest().authenticated();
 
         http.addFilterBefore(authenticationFiler(), UsernamePasswordAuthenticationFilter.class);
