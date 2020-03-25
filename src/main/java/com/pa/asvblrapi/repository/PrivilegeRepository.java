5074e5b4777071baed2b30a141a240058f4bfe1c
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-03-25 13:24:06 +0100

    UPD / ADD Update repository & add privilege repository

---
+++ src/main/java/com/pa/asvblrapi/repository/PrivilegeRepository.java
@@ -0,0 +1,12 @@
+package com.pa.asvblrapi.repository;
+
+import com.pa.asvblrapi.entity.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivilegeRepository extends JpaRepository<Privilege, Long> {
+
+    Privilege findByName(String name);
+
+    @Override
+    void delete(Privilege privilege);
+}
--- src/main/java/com/pa/asvblrapi/repository/RoleRepository.java
+++ src/main/java/com/pa/asvblrapi/repository/RoleRepository.java
@@ -1,11 +1,12 @@
 package com.pa.asvblrapi.repository;
 
-import com.pa.asvblrapi.entity.ERole;
 import com.pa.asvblrapi.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(ERole name);
+
+    Role findByName(String name);
+
+    @Override
+    void delete(Role role);
 }
