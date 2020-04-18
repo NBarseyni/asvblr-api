3a44b3751bc7da33e0ccfe1e56bf883f76f17c35
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-04-18 16:05:08 +0200

    FIX response status paymentmodeException

--- src/main/java/com/pa/asvblrapi/exception/PaymentModeNotFoundException.java
+++ src/main/java/com/pa/asvblrapi/exception/PaymentModeNotFoundException.java
@@ -1,5 +1,9 @@
 package com.pa.asvblrapi.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class PaymentModeNotFoundException extends RuntimeException{
     public PaymentModeNotFoundException(Long id) {
         super("Could not find paymentMode with id = " + id);
