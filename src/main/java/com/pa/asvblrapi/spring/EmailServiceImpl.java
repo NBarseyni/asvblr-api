fd19a358d380a2d0406d746257d3966946fa8302
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-05-03 15:09:35 +0200

    ADD send email with ThymeleafTemplate / no image in mail

--- src/main/java/com/pa/asvblrapi/spring/EmailServiceImpl.java
+++ src/main/java/com/pa/asvblrapi/spring/EmailServiceImpl.java
@@ -1,16 +1,30 @@
 package com.pa.asvblrapi.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.Map;
 
 @Component
 public class EmailServiceImpl {
 
     @Autowired
     public JavaMailSender emailSender;
+
+    @Autowired
+    private SpringTemplateEngine thymeleafTemplateEngine;
+
+    @Value("classpath:/mail-logo.png")
+    Resource resourceFile;
 
     public void sendSimpleMessage(String to, String subject, String text) {
         try {
@@ -23,5 +37,25 @@ public class EmailServiceImpl {
         catch (MailException exception) {
             exception.printStackTrace();
         }
+    }
+
+    public void sendMessageUsingThymeleafTemplate(String to, String subject, Map<String, Object> templateModel)
+        throws MessagingException {
+
+        Context thymeleafContext = new Context();
+        thymeleafContext.setVariables(templateModel);
+        String htmlBody = thymeleafTemplateEngine.process("template-new-subscription.html", thymeleafContext);
+
+        sendHtmlMessage(to, subject, htmlBody);
+    }
+
+    private void sendHtmlMessage(String to, String subject, String htmlBody) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlBody, true);
+        //helper.addInline("attachment.png", resourceFile);
+        emailSender.send(message);
     }
 }
