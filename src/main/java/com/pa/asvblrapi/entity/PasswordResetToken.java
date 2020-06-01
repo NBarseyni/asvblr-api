5d1e05b83a856fe7852712b2006d3feae99b1187
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-06-01 20:00:38 +0200

    ADD PasswordResetToken entity with its repository

---
+++ src/main/java/com/pa/asvblrapi/entity/PasswordResetToken.java
@@ -0,0 +1,43 @@
+package com.pa.asvblrapi.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class PasswordResetToken {
+
+    private static final int EXPIRATION_TIME = 60 * 24;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Date expirationDate;
+
+    public PasswordResetToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+        this.expirationDate = calculateExpiryDate(EXPIRATION_TIME);
+    }
+
+    private Date calculateExpiryDate(final int expiryTimeInMinutes) {
+        final Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(new Date().getTime());
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+}
---
+++ src/main/java/com/pa/asvblrapi/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,11 @@
+package com.pa.asvblrapi.repository;
+
+import com.pa.asvblrapi.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    PasswordResetToken findByToken(String token);
+}
