47755982fa37f18c0377cbe8dae407809aafcb0a
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-06-13 11:06:05 +0200

    ADD DriveMapper with its tests

---
+++ src/main/java/com/pa/asvblrapi/mapper/DriveMapper.java
@@ -0,0 +1,19 @@
+package com.pa.asvblrapi.mapper;
+
+import com.pa.asvblrapi.dto.DriveDto;
+import com.pa.asvblrapi.entity.Drive;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {})
+public interface DriveMapper extends EntityMapper<DriveDto, Drive> {
+    DriveMapper instance = Mappers.getMapper(DriveMapper.class);
+
+    @Mapping(source = "driver.id", target = "idDriver")
+    @Mapping(source = "match.id", target = "idMatch")
+    DriveDto toDto(Drive drive);
+    List<DriveDto> toDto(List<Drive> drives);
+}
---
+++ src/test/java/com/pa/asvblrapi/mapper/DriveMappingTests.java
@@ -0,0 +1,35 @@
+package com.pa.asvblrapi.mapper;
+
+import com.pa.asvblrapi.dto.DriveDto;
+import com.pa.asvblrapi.entity.Drive;
+import com.pa.asvblrapi.entity.Match;
+import com.pa.asvblrapi.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class DriveMappingTests {
+
+    @Test
+    public void should_map_drive_to_dto() {
+        User user = new User();
+        user.setId((long)2);
+        Match match = new Match();
+        match.setId((long)3);
+        Drive drive = new Drive((long)1, "address", 3, 3, user, match, new ArrayList<>());
+
+        DriveDto driveDto = DriveMapper.instance.toDto(drive);
+
+        assertThat(driveDto).isNotNull();
+        assertThat(driveDto.getId()).isEqualTo(drive.getId());
+        assertThat(driveDto.getAddress()).isEqualTo(drive.getAddress());
+        assertThat(driveDto.getNbTotalPlaces()).isEqualTo(drive.getNbTotalPlaces());
+        assertThat(driveDto.getNbFreePlaces()).isEqualTo(drive.getNbFreePlaces());
+        assertThat(driveDto.getIdDriver()).isEqualTo(user.getId());
+        assertThat(driveDto.getIdMatch()).isEqualTo(match.getId());
+    }
+}
