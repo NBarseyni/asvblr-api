c0e7935c8ce615e860779998480afe38021729c4
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-06-13 11:03:40 +0200

    ADD Drive and DriveDto

---
+++ src/main/java/com/pa/asvblrapi/dto/DriveDto.java
@@ -0,0 +1,22 @@
+package com.pa.asvblrapi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+public class DriveDto {
+    private Long id;
+    @NotBlank
+    private String address;
+    @NotNull
+    private int nbTotalPlaces;
+    private int nbFreePlaces;
+    @NotNull
+    private Long idDriver;
+    @NotNull
+    private Long idMatch;
+}
---
+++ src/main/java/com/pa/asvblrapi/entity/Drive.java
@@ -0,0 +1,38 @@
+package com.pa.asvblrapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Drive {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    private String address;
+    @NotNull
+    private int nbTotalPlaces;
+    @NotNull
+    private int nbFreePlaces;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User driver;
+    @ManyToOne
+    @JoinColumn(name = "match_id", nullable = false)
+    private Match match;
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "drives_users",
+            joinColumns = @JoinColumn(name = "drive_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> passengers;
+}
