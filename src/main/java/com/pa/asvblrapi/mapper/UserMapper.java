a260cc7310646d646499c66affc4ba1e43937216
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-06-17 16:39:05 +0200

    ADD UserMapper for method toDto
    It's don't extend EntityMapper because we don't need toEntity for this
    mapper

---
+++ src/main/java/com/pa/asvblrapi/mapper/UserMapper.java
@@ -0,0 +1,43 @@
+package com.pa.asvblrapi.mapper;
+
+import com.pa.asvblrapi.dto.UserDto;
+import com.pa.asvblrapi.entity.Privilege;
+import com.pa.asvblrapi.entity.Role;
+import com.pa.asvblrapi.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mapper(componentModel = "spring", uses = {})
+public interface UserMapper {
+    UserMapper instance = Mappers.getMapper(UserMapper.class);
+
+    default UserDto toDto(User user) {
+        List<String> roles = new ArrayList<>();
+        List<String> privileges = new ArrayList<>();
+        for (Role role :
+                user.getRoles()) {
+            roles.add(role.getName());
+            for (Privilege privilege :
+                    role.getPrivileges()) {
+                if (!privileges.contains(privilege.getName())) {
+                    privileges.add(privilege.getName());
+                }
+            }
+        }
+
+        return new UserDto(
+                user.getId(),
+                user.getUsername(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.isPasswordChanged(),
+                roles,
+                privileges);
+    }
+
+    List<UserDto> toDto(List<User> users);
+}
