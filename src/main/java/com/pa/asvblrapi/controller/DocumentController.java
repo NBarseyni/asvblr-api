2e3e24ad5ee44f33b2d85d2a366b280067d2f327
Nicolas Barseyni <nicolas.barseyni@protonmail.com>
2020-06-28 16:56:26 +0200

    ADD get my documents
    Find documents by idUser with SecurityContext

--- src/main/java/com/pa/asvblrapi/controller/DocumentController.java
+++ src/main/java/com/pa/asvblrapi/controller/DocumentController.java
@@ -1,10 +1,18 @@
 package com.pa.asvblrapi.controller;
 
 import com.pa.asvblrapi.entity.Document;
+import com.pa.asvblrapi.entity.User;
 import com.pa.asvblrapi.exception.DocumentNotFoundException;
+import com.pa.asvblrapi.exception.UserIsNotPlayerException;
 import com.pa.asvblrapi.service.DocumentService;
+import com.pa.asvblrapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -13,9 +21,26 @@ public class DocumentController {
     @Autowired
     private DocumentService documentService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/{id}")
     public Document getDocument(@PathVariable Long id) {
         return this.documentService.getDocument(id)
                 .orElseThrow(() -> new DocumentNotFoundException(id));
+    }
+
+    @GetMapping("/my-documents")
+    public ResponseEntity<Object> getMyDocuments() {
+        try {
+            User user = this.userService.getUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+            List<Document> documents = this.documentService.getMyDocuments(user);
+            return ResponseEntity.status(HttpStatus.OK).body(documents);
+        } catch (UserIsNotPlayerException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
     }
 }
--- src/main/java/com/pa/asvblrapi/service/DocumentService.java
+++ src/main/java/com/pa/asvblrapi/service/DocumentService.java
@@ -1,8 +1,11 @@
 package com.pa.asvblrapi.service;
 
 import com.pa.asvblrapi.entity.Document;
+import com.pa.asvblrapi.entity.Player;
 import com.pa.asvblrapi.entity.Subscription;
+import com.pa.asvblrapi.entity.User;
 import com.pa.asvblrapi.exception.DocumentNotFoundException;
+import com.pa.asvblrapi.exception.UserIsNotPlayerException;
 import com.pa.asvblrapi.repository.DocumentRepository;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +13,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,10 +25,27 @@ public class DocumentService {
     @Autowired
     private DocumentRepository documentRepository;
 
+    @Autowired
+    private PlayerService playerService;
+
     private final String UPLOADED_FOLDER = "src/main/resources/public/documents/";
 
     public Optional<Document> getDocument(Long id) {
         return this.documentRepository.findById(id);
+    }
+
+    public List<Document> getMyDocuments(User user) throws UserIsNotPlayerException {
+        Player player = user.getPlayer();
+        if (player == null) {
+            throw new UserIsNotPlayerException(user.getId());
+        }
+        Subscription subscription = this.playerService.getLastSubscription(player.getId());
+
+        List<Document> documents = new ArrayList<>();
+        documents.add(subscription.getCNI());
+        documents.add(subscription.getIdentityPhoto());
+        documents.add(subscription.getMedicalCertificate());
+        return documents;
     }
 
     public Document createDocument(MultipartFile file, String username) throws IOException {
